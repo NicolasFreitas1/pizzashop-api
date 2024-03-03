13bf06915a0b5db262f416e1921a2b211a7db7c6
root <root@DESKTOP-35PJVHJ>
2024-03-03 17:55:31 -0300

    add authentication and email transporter

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "drizzle-kit";
-import { env } from "./src/env.ts";
+import type { Config } from 'drizzle-kit'
+import { env } from './src/env.ts'
 
 export default {
-  schema: "./src/db/schema/index.ts",
-  out: "./drizzle",
-  driver: "pg",
+  schema: './src/db/schema/index.ts',
+  out: './drizzle',
+  driver: 'pg',
   dbCredentials: {
     connectionString: env.DATABASE_URL,
   },
-} satisfies Config;
+} satisfies Config
